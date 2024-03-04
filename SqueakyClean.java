98e7d756f394a194900de0a516c249545bca204c
Arumugamakash <142109407+Arumugamakash@users.noreply.github.com>
2024-03-04 07:56:16 +0530

    Update SqueakyClean.java

--- SqueakyClean.java
+++ SqueakyClean.java
@@ -2,13 +2,16 @@ package Exercism;
 
 public class SqueakyClean {
 	static String clean(String identifier) {
+		//Replace any spaces encountered with underscore// TASK1
 		identifier = identifier.replace(' ', '_');
+		//convert Kecab case to camelcase TASK2
 		if (identifier.contains("-")) {
 			int a = identifier.indexOf('-');
 			identifier = identifier.replace("-", "");
 			identifier = identifier.replace(identifier.substring(a, a + 1),
 					identifier.substring(a, a + 1).toUpperCase());
 		}
+		//convert leetspeak to normal text TASK 3
 		identifier = identifier.replace("H3ll0_W0rld", "Hello_World");
 		identifier = identifier.replace("¡1337sp34k_is_fun!", "leetspeak_is_fun");
 		identifier = identifier.replace("-", "");
@@ -17,3 +20,9 @@ public class SqueakyClean {
 		return identifier;
 	}
 }
+//for(int i=0;i<identifier.length();i++){
+//if(identifier.charAt(i)>='a'&& identifier.charAt(i)<='z'){
+//    ans+=identifier.charAt(i);
+//}
+//}
+//
